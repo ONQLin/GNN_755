3c048a3d83cb6ee7d93a71c72e0a3dafedf31d15
ONQLin <oliver12125@sina.com>
2023-02-21 18:38:10 -0600

    follow up the req, add reg for each layer

--- rtl/dnn_top.sv
+++ rtl/dnn_top.sv
@@ -5,9 +5,7 @@
 // estimate registers: mid or last layer [l20, l21, l22, l23] or [out0, out1] can be more efficient
 // hand-shaking signals: synchronous with registers
 
-module dnn_top#(
-    parameter reg_pos = 0
-)
+module dnn_top
 (
     input signed[4:0] x0, x1, x2, x3, w04, w05, w06, w07, w14, w15, w16, w17, w24, w25, w26, w27, w34,          // weights and inputs
     w35, w36, w37, w48, w58, w49, w59, w68, w69, w78, w79,
@@ -33,18 +31,32 @@ module dnn_top#(
     endgenerate
 
     logic signed[10:0] s1[0:3];                                                            // results of 4 5 6 7 (index + 4)
+
     generate            // get sums of layer1
         for(i = 0; i < 4; i=i+1) begin
-            assign s1[i] = p1[0][i] + p1[1][i] + p1[2][i] + p1[3][i];
+            always_ff @(posedge clk) begin
+                if(in_ready)
+                    s1[i] <= p1[0][i] + p1[1][i] + p1[2][i] + p1[3][i];
+            end
         end
     endgenerate
+
+    logic in_rdy_1
+    always_ff @(posedge clk) begin
+        in_rdy_1 <= in_ready;
+    end
 
     logic signed[10:0] s_o1[0:3];    
     generate            // Relu
         for (i = 0; i < 4; i=i+1) begin
-            assign s_o1[i] = Relu(s1[i]);
+            always_ff @(posedge clk) begin
+                if(in_rdy_1)
+                    s_o1 <= (s1[i][10]) ? 10'd0 : {1'b0, si[i][9:0]};
+            end
         end
     endgenerate
+
+
 
     wire signed[4:0] w2[0:3][0:1] = '{'{w48, w49},'{w58, w59},
     '{w68, w69},'{w78, w79}};                                                           //the weights of layer2 w2[0][1]-->w49  w2[2][0]-->w68  (+4 for the 1st index and +8 for the 2nd index)
@@ -58,30 +70,27 @@ module dnn_top#(
         end
     endgenerate
 
+    logic in_rdy_2
+    always_ff @(posedge clk) begin
+        in_rdy_2 <= in_rdy_1;
+    end
+
     wire signed[16:0] s2[0:1];  
 
     generate            // get sums of layer1
         for(i = 0; i < 2; i=i+1) begin
-            assign s2[i] = p2[0][i] + p2[1][i] + p2[2][i] + p2[3][i];
+            always_ff @(posedge clk) begin
+                if(in_rdy_2)
+                    s2[i] <= p2[0][i] + p2[1][i] + p2[2][i] + p2[3][i];
+            end
         end
     endgenerate
 
     always_ff @(posedge clk) begin
-        if(in_ready) begin
-            out0 <= s2[0];
-            out1 <= s2[1];
-            out0_ready <= 1;
-            out1_ready <= 1;
-        end else begin
-            out0 <= out0;
-            out1 <= out1;
-            out0_ready <= 0;
-            out1_ready <= 0;
-        end
+        out0_ready <= in_rdy_2;
+        out1_ready <= in_rdy_2;
     end
 
-    function signed[10:0] Relu(input signed[10:0] sum_in);
-        Relu = (sum_in[10]) ? 10'd0 : {1'b0, sum_in[9:0]};
-    endfunction
+    assign {out0, out1} = {s2[0],s2[1]};    
     
 endmodule
